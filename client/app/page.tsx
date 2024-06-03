f7a1fca39c034defed4c2621f61dd6c8be22e188
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-06-03 10:23:45 +0530

    Created the basic form structure

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,47 @@
+import { Label,TextInput,Button, Checkbox } from "flowbite-react";
+
 export default function Home() {
   return (
     <main>
-      This is the main page of the app
+      <div className="flex justify-center content-end mt-28">
+        <form className="flex flex-col gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email2" value="Your email" />
+            </div>
+            <TextInput
+              id="email2"
+              type="email"
+              placeholder="name@flowbite.com"
+              required={true}
+              shadow={true}
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password2" value="Your password" />
+            </div>
+            <TextInput
+              id="password2"
+              type="password"
+              required={true}
+              shadow={true}
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="repeat-password" value="Repeat password" />
+            </div>
+            <TextInput
+              id="repeat-password"
+              type="password"
+              required={true}
+              shadow={true}
+            />
+          </div>          
+          <Button type="submit">Register new account</Button>
+        </form>
+      </div>
     </main>
   );
 }

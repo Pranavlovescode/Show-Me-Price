f94378963d7f02bee4ce6561e04cd05412a93b8a
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2024-06-03 13:23:36 +0530

    Added styling to the page with background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,48 @@
-import { Label,TextInput,Button, Checkbox } from "flowbite-react";
+import { Label, TextInput, Button } from "flowbite-react";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex justify-center content-end mt-28 ">
-        <div className="border border-gray-300 rounded-md shadow-md">
-        <form className="flex flex-col gap-4 my-5   mx-28">
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="email2" value="Your email" />
+    <main className="h-screen bg-[url(../public/bg.jpg)] bg-center bg-cover bg-blend-overlay z-0">
+      <h1 className="text-center pt-14 text-4xl font-extrabold text-gray-400">Mumbai House Price Prediction</h1>
+      <div className="flex justify-center content-center pt-20">
+        <div className="border-none rounded-md shadow-md bg-white bg-opacity-50 p-6 z-10">
+          <form className="flex flex-col gap-4 my-5 px-28">
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email2" value="Your email" />
+              </div>
+              <input
+                className="bg-gray-300 border-none rounded-md p-2 w-full"
+                id="email2"
+                type="email"
+                placeholder="name@flowbite.com"
+                required={true}
+              />
             </div>
-            <TextInput
-              id="email2"
-              type="email"
-              placeholder="name@flowbite.com"
-              required={true}
-              shadow={true}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="password2" value="Your password" />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="password2" value="Your password" />
+              </div>
+              <input
+                className="bg-gray-300 border-none rounded-md p-2 w-full"
+                id="password2"
+                type="password"
+                required={true}
+              />
             </div>
-            <TextInput
-              id="password2"
-              type="password"
-              required={true}
-              shadow={true}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="repeat-password" value="Repeat password" />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="repeat-password" value="Repeat password" />
+              </div>
+              <input
+                className="bg-gray-300 border-none rounded-md p-2 w-full"
+                id="repeat-password"
+                type="password"
+                required={true}
+              />
             </div>
-            <TextInput
-              id="repeat-password"
-              type="password"
-              required={true}
-              shadow={true}
-            />
-          </div>          
-          <Button type="submit">Register new account</Button>
-        </form>
+            <Button type="submit" color='blue'>Register new account</Button>
+          </form>
         </div>
       </div>
     </main>
